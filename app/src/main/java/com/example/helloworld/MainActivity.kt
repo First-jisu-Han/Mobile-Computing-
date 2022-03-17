bd1f25f2a281a461a738aaba64544f99294b2e3d
Jisu Han <weblite@naver.com>
2022-03-17 20:32:54 +0900

    간단한 버튼 클릭 -> 메세지 나오도록 동작 변수2개

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -6,15 +6,21 @@ import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button1: Button
+    lateinit var name: Button  // 버튼 id로 버튼 변수 선언 - name
+    lateinit var id: Button  //  버튼 id러 버튼 변수 선언  - id
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1  = findViewById<Button>(R.id.button1)
-        button1.setOnClickListener{
-            Toast.makeText(applicationContext,"Button clicked",Toast.LENGTH_SHORT).show()
+        name = findViewById<Button>(R.id.name)
+        id= findViewById<Button>(R.id.id)
+        name.setOnClickListener{
+            Toast.makeText(applicationContext,"JISU HAN",Toast.LENGTH_LONG).show()
         }
+        id.setOnClickListener{
+            Toast.makeText(applicationContext,"60172390",Toast.LENGTH_LONG).show()
+        }
+
 
     }
 }
