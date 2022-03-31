c3eddd90bab667018cbb84cc351b9207ce48d6cd
Jisu Han <weblite@naver.com>
2022-04-01 01:54:23 +0900

    Excepiton 추가로 생각해 보기

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -10,9 +10,9 @@ import java.lang.NumberFormatException
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding   // viewBinding 을 사용하기 위해 클래스를 저장
     override fun onCreate(savedInstanceState: Bundle?) {
-        var num1: Int
-        var num2: Int
-        var result: Int
+        var num1:Double
+        var num2:Double
+        var result:Double
         super.onCreate(savedInstanceState)
         binding =
             ActivityMainBinding.inflate(layoutInflater) // xml에 있는 레이아웃정보들을 받아올 수 있게 inflate 함수실행
@@ -20,45 +20,53 @@ class MainActivity : AppCompatActivity() {
 
         binding.add.setOnClickListener {
             try {
-                num1 = binding.num1.text.toString().toInt()
-                num2 = binding.num2.text.toString().toInt()
+                num1 = binding.num1.text.toString().toDouble()
+                num2 = binding.num2.text.toString().toDouble()
                 result = num1 + num2
                 binding.result.text = getString(R.string.result, result.toString())
             } catch (e: NumberFormatException) {
                 Toast.makeText(applicationContext,"숫자를 입력하시오",Toast.LENGTH_SHORT).show()
+            } catch (e:StackOverflowError){
+                Toast.makeText(applicationContext,"너무 큰 숫자입니다.줄이십쇼",Toast.LENGTH_SHORT).show()
             }
         }
         binding.sub.setOnClickListener {
             try {
-                num1 = binding.num1.text.toString().toInt()
-                num2 = binding.num2.text.toString().toInt()
+                num1 = binding.num1.text.toString().toDouble()
+                num2 = binding.num2.text.toString().toDouble()
                 result = num1 - num2
                 binding.result.text = getString(R.string.result, result.toString())
             } catch (e: NumberFormatException) {
                 Toast.makeText(applicationContext,"숫자를 입력하시오",Toast.LENGTH_SHORT).show()
+            }catch (e:StackOverflowError){
+                Toast.makeText(applicationContext,"너무 큰 숫자입니다.줄이십쇼",Toast.LENGTH_SHORT).show()
             }
         }
         binding.mul.setOnClickListener {
             try {
-                num1 = binding.num1.text.toString().toInt()
-                num2 = binding.num2.text.toString().toInt()
+                num1 = binding.num1.text.toString().toDouble()
+                num2 = binding.num2.text.toString().toDouble()
                 result = num1 * num2
                 binding.result.text = getString(R.string.result, result.toString())
             } catch (e: NumberFormatException) {
                 Toast.makeText(applicationContext,"숫자를 입력하시오",Toast.LENGTH_SHORT).show()
+            }catch (e:StackOverflowError){
+                Toast.makeText(applicationContext,"너무 큰 숫자입니다.줄이십쇼",Toast.LENGTH_SHORT).show()
             }
         }
 
         binding.div.setOnClickListener {
             try {
-                num1 = binding.num1.text.toString().toInt()
-                num2 = binding.num2.text.toString().toInt()
+                num1 = binding.num1.text.toString().toDouble()
+                num2 = binding.num2.text.toString().toDouble()
                 result = num1 / num2
                 binding.result.text = getString(R.string.result, result.toString())
             } catch (e: NumberFormatException) {
                 Toast.makeText(applicationContext,"숫자를 입력하시오",Toast.LENGTH_SHORT).show()
             } catch (e: ArithmeticException) {
                 Toast.makeText(applicationContext,"나누는 수는 0이될 수 없습니다 다시입력하시오",Toast.LENGTH_SHORT).show()
+            }catch (e:StackOverflowError){
+                Toast.makeText(applicationContext,"너무 큰 숫자입니다.줄이십쇼",Toast.LENGTH_SHORT).show()
             }
         }
     }
