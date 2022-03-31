fad890b66acfe45253e1c2a446068dc55e5a549d
Jisu Han <weblite@naver.com>
2022-04-01 01:18:05 +0900

    Exception Handling 하기 - ArithmaticException , NumberFormatException 핸들링 - 예외 발생시 Toast.makeText로 팝업메세지 띄우도록 설정했음

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,42 +2,65 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.helloworld.databinding.ActivityMainBinding
+import java.lang.ArithmeticException
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding   // viewBinding 을 사용하기 위해 클래스를 저장
     override fun onCreate(savedInstanceState: Bundle?) {
-        var num1:Int
-        var num2:Int
-        var result:Int
+        var num1: Int
+        var num2: Int
+        var result: Int
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater) // xml에 있는 레이아웃정보들을 받아올 수 있게 inflate 함수실행
+        binding =
+            ActivityMainBinding.inflate(layoutInflater) // xml에 있는 레이아웃정보들을 받아올 수 있게 inflate 함수실행
         setContentView(binding.root)  // viewBinding을 위해서 수정
 
-        binding.add.setOnClickListener{
-            num1=binding.num1.ㅅtoString().toInt()
-            num2=binding.num2.toString().toInt()
-            result=num1+num2
-            binding.result.text=getString(R.string.result,result.toString())
+        binding.add.setOnClickListener {
+            try {
+                num1 = binding.num1.text.toString().toInt()
+                num2 = binding.num2.text.toString().toInt()
+                result = num1 + num2
+                binding.result.text = getString(R.string.result, result.toString())
+            } catch (e: NumberFormatException) {
+                Toast.makeText(applicationContext,"숫자를 입력하시오",Toast.LENGTH_SHORT).show()
+            }
         }
-        binding.sub.setOnClickListener{
-            num1=binding.num1.toString().toInt()
-            num2=binding.num2.toString().toInt()
-            result=num1-num2
-            binding.result.text=getString(R.string.result,result.toString())
+        binding.sub.setOnClickListener {
+            try {
+                num1 = binding.num1.text.toString().toInt()
+                num2 = binding.num2.text.toString().toInt()
+                result = num1 - num2
+                binding.result.text = getString(R.string.result, result.toString())
+            } catch (e: NumberFormatException) {
+                Toast.makeText(applicationContext,"숫자를 입력하시오",Toast.LENGTH_SHORT).show()
+            }
         }
-        binding.mul.setOnClickListener{
-            num1=binding.num1.toString().toInt()
-            num2=binding.num2.toString().toInt()
-            result=num1*num2
-            binding.result.text=getString(R.string.result,result.toString())
-        }
-        binding.div.setOnClickListener{
-            num1=binding.num1.toString().toInt()
-            num2=binding.num2.toString().toInt()
-            result=num1/num2
-            binding.result.text=getString(R.string.result,result.toString())
+        binding.mul.setOnClickListener {
+            try {
+                num1 = binding.num1.text.toString().toInt()
+                num2 = binding.num2.text.toString().toInt()
+                result = num1 * num2
+                binding.result.text = getString(R.string.result, result.toString())
+            } catch (e: NumberFormatException) {
+                Toast.makeText(applicationContext,"숫자를 입력하시오",Toast.LENGTH_SHORT).show()
+            }
         }
 
+        binding.div.setOnClickListener {
+            try {
+                num1 = binding.num1.text.toString().toInt()
+                num2 = binding.num2.text.toString().toInt()
+                result = num1 / num2
+                binding.result.text = getString(R.string.result, result.toString())
+            } catch (e: NumberFormatException) {
+                Toast.makeText(applicationContext,"숫자를 입력하시오",Toast.LENGTH_SHORT).show()
+            } catch (e: ArithmeticException) {
+                Toast.makeText(applicationContext,"나누는 수는 0이될 수 없습니다 다시입력하시오",Toast.LENGTH_SHORT).show()
+            }
         }
     }
+}
+
